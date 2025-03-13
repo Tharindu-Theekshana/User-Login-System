2f9302aeca0457ed01e494f6bd9af3464a34e0c5
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-03-13 17:28:40 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
+
 export default function Login() {
   return (
-    <div>
-      <div>
-        <form >
-          <div>
+    <div class='flex place-content-center justify-center'>
+      <div class='flex  bg-blend-color-burn bg-green-100 w-100 place-content-center h-150 mt-20 ' >
+        <div>
+          <img src='' alt="" />
+        </div>
+        <form class='flex flex-col gap-4'>
+          <div class='text-5xl text-green-700 p-5 flex place-content-center mt-5'>
             Login User
           </div>
-          <div>
+          <div>  
           <input placeholder='Enter Username'/>
           </div>
           <div>
