7aa290e20441065bbc17c1a85257e90922448199
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-03-13 19:41:02 +0530

    created login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import login from '../images/login.jpg'
 
 
 export default function Login() {
   return (
-    <div class='flex place-content-center justify-center'>
-      <div class='flex  bg-blend-color-burn bg-green-100 w-100 place-content-center h-150 mt-20 ' >
-        <div>
-          <img src='' alt="" />
+    <div className='flex justify-center h-screen bg-purple-200'>
+      <div className='  bg-white w-[600px]  place-content-center h-[400px] mt-30 shadow-lg rounded-2xl grid grid-cols-2' >
+        <div className='w-full mt-5'>
+          <img src={login} alt="loginImage"/>
         </div>
-        <form class='flex flex-col gap-4'>
-          <div class='text-5xl text-green-700 p-5 flex place-content-center mt-5'>
+        <form className='flex flex-col justify-center px-8'>
+          <div className='text-3xl font-bold text-purple-800 p-5 flex text-center mb-6'>
             Login User
           </div>
           <div>  
-          <input placeholder='Enter Username'/>
+          <input type='text' placeholder='Enter Username' className='w-full p-2 border border-gray-300 rounded-md mb-4'/>
           </div>
           <div>
-          <input placeholder='Enter Password'/>
+          <input type='password' placeholder='Enter Password' className='w-full p-2 border border-gray-300 rounded-md mb-4'/>
           </div>
           <div>
-            <button type='submit'>Login</button>
+            <button type='submit' className='w-full bg-purple-500 rounded-lg text-white py-2 hover:bg-purple-600 transition hover:scale-105'>Login</button>
           </div>
+         <div className='text-sm text-gray-500 flex justify-end mr-2 mt-3 hover:underline hover:text-gray-800'><button>Sign Up</button></div>
         </form>
       </div>
     </div>
