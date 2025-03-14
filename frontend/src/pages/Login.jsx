95bdf0ac7013015eb3a1eb002e436e132dbda459
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-03-14 18:47:11 +0530

    added form validation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,38 +1,65 @@
 import React, { useState } from 'react'
 import login from '../images/login.jpg'
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
-
+const schema = yup.object().shape({
+  signupUsername : yup.string().email().required("You must enter email."),
+  loginUsername : yup.string().email().required("You must enter email."),
+  signupPassword : yup.string().required("You must enter password").min(3),
+  confirmPassword : yup.string().oneOf([yup.ref("signupPassword"),null],"Passwords do not match").required("You must confirm your password."),
+  loginPassword : yup.string().required("You must enter password").min(3),
+  
+}); 
 
 export default function Login() {
+                                     
+                                           
+  const {register, handleSubmit,formState: {errors}} = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const [isSignup,setIsSignup] = useState(false);
+  const [isSignup,setIsSignup] = useState(true);
   const [loginUsername,setLoginUsername] = useState("");
   const [loginPassword,setLoginPassword] = useState("");
   const [signupUsername,setSignupUsername] = useState("");
   const [signupPassword,setSignupPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleSignupSubmit = (e) => {
-
-    e.preventDefault();
-    console.log(signupUsername,signupPassword,confirmPassword + " signUp sccessfull");
-    setIsSignup(true);
-    setSignupUsername("");
-    setSignupPassword("");
-    setConfirmPassword("");
-
-
+  const handleUsernameChange = (e) => {
+   
+    if(isSignup){
+      setLoginUsername(e.target.value);
+    }else{
+      setSignupUsername(e.target.value);
+    }
   }
 
-  const handleLoginSubmit = (e) => {
+  const handlePasswordChange = (e) => {
 
-    e.preventDefault();
-    console.log(loginUsername,loginPassword+ " login sccessfull");
-    setLoginUsername("");
-    setLoginPassword("");
+    if(isSignup){
+      setLoginPassword(e.target.value);
+    }else{
+      setSignupPassword(e.target.value);
+    }
+  }
+
   
-    
+ 
+  const whenSubmited = (data, e) => {
+  e.preventDefault();
+
+  if (!isSignup) {
+   
+    console.log(data.signupUsername, data.signupPassword, data.confirmPassword + " signUp successful");
+    setIsSignup(true);
+  } else {
+    console.log(data.loginUsername, data.loginPassword, "login successful");
   }
+ 
+};
+  
 
   return (
     <div className='flex justify-center h-screen bg-purple-200'>
@@ -40,19 +67,35 @@ export default function Login() {
         <div className={`w-full ${isSignup ? 'mt-7': 'mt-14'}`}>
           <img src={login} alt="loginImage"/>
         </div>
-        <form className='flex flex-col justify-center px-8'>
+        <form className='flex flex-col justify-center px-8'  onSubmit={handleSubmit(whenSubmited)}>
           <div className='text-3xl font-bold text-purple-800 p-5 flex text-center mb-6'>
             {isSignup? "Login" : "Sign Up"}
           </div>
           <div>  
-          <input value={isSignup? loginUsername : signupUsername} onChange={isSignup? (e) => {setLoginUsername(e.target.value)} : (e) => {setSignupUsername(e.target.value)}} type='text' placeholder='Enter Username' className='w-full p-2 border border-gray-300 rounded-md mb-4'/>
+          <input 
+          onChange={handleUsernameChange} 
+          {...register(isSignup? "loginUsername" : "signupUsername")} 
+          type='text' placeholder='Enter Username' 
+          className='w-full p-2 border border-gray-300 rounded-md mb-0.5'/>
+          <p className='text-xs text-red-500 ml-3 font-[Opens_Sans]'>{isSignup? errors.loginUsername?.message : errors.signupUsername?.message}</p>
           </div>
           <div>
-          <input value={isSignup? loginPassword : signupPassword} onChange={isSignup? (e) => {setLoginPassword(e.target.value)} : (e) => {setSignupPassword(e.target.value)}} type='password' placeholder='Enter Password' className='w-full p-2 border border-gray-300 rounded-md mb-4'/>
-          {!isSignup && <input value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}} type='password' placeholder='Confirm Password' className='w-full p-2 border border-gray-300 rounded-md mb-4'/>}
+          <input 
+          onChange={handlePasswordChange} 
+          {...register(isSignup? "loginPassword" : "signupPassword")} 
+          type='password' placeholder='Enter Password' 
+          className='w-full p-2 border border-gray-300 rounded-md mb-0.5 mt-2'/>
+          <p className='text-xs text-red-500 ml-3 font-[Opens_Sans]'>{isSignup? errors.loginPassword?.message : errors.signupPassword?.message}</p>
+          {!isSignup && 
+          <input 
+          onChange={(e) => {setConfirmPassword(e.target.value)}} 
+          {...register("confirmPassword")} type='password' 
+          placeholder='Confirm Password' 
+          className='w-full p-2 border border-gray-300 rounded-md mb-0.5 mt-2'/>}
+          {!isSignup &&<p className='text-xs text-red-500 ml-3 font-[Opens_Sans]'>{errors.confirmPassword?.message}</p>}
           </div>
           <div>
-            <button onClick={isSignup? handleLoginSubmit : handleSignupSubmit} type='submit' className='w-full bg-purple-500 rounded-lg text-white py-2 hover:bg-purple-600 transition hover:scale-105'>{isSignup? "Login":"Sign Up"}</button>
+            <button type='submit' className='w-full bg-purple-500 rounded-lg text-white py-2 hover:bg-purple-600 transition hover:scale-105 mt-2'>{isSignup? "Login":"Sign Up"}</button>
           </div>
          <div className='flex justify-end mr-2 mt-3 '><span className='mr-10 mt-0.5 text-gray-500 text-xs'>{isSignup? "Don't have an account?" : "Already have an account? "}</span><button type='button' className='hover:underline hover:text-gray-800 text-gray-700 text-sm' onClick={() => setIsSignup(!isSignup)}>{isSignup? "Sign Up" : "Login"}</button></div>
         </form>
